351e34f04f1bab3e586905d609d7a4cf0c3b8106
João Pedro <jpedrodaibello1999@outlook.com>
2024-04-25 13:49:58 -0300

    feat(validation_vs_coupling_challenge): create Product validation

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
@@ -19,29 +20,8 @@ export default class Product extends Entity implements ProductInterface {
 		}
 	}
 
-	validate(): boolean {
-		if (this.id.length === 0) {
-			this.notification.addError({
-				context: "product",
-				message: "ID is required"
-			});
-		}
-
-		if (this._name.length === 0) {
-			this.notification.addError({
-				context: "product",
-				message: "Name is required"
-			});
-		}
-
-		if (this._price <= 0) {
-			this.notification.addError({
-				context: "product",
-				message: "Price must be greater than 0",
-			});
-		}
-
-		return true;
+	validate() {
+		ProductValidatorFactory.create().validate(this);
 	}
 
 	get name(): string {
---
+++ src/domain/product/validator/product.yup.validator.ts
@@ -0,0 +1,30 @@
+import ValidatorInterface from "../../@shared/validator/validator.interface";
+import Product from "../entity/product";
+import * as yup from "yup";
+
+export default class ProductYupValidator implements ValidatorInterface<Product> {
+	validate(entity: Product): void {
+		try {
+			yup.object().shape({
+				id: yup.string().required("ID is required"),
+				name: yup.string().required("Name is required"),
+				price: yup.number().moreThan(0, "Price must be greater than 0")
+			}).validateSync({
+				id: entity.id,
+				name: entity.name,
+				price: entity.price
+			}, {
+				abortEarly: false
+			});
+		} catch (errors) {
+			const e = errors as yup.ValidationError;
+
+			e.errors.forEach((error) => {
+				entity.notification.addError({
+					message: error,
+					context: "product",
+				});
+			});
+		}
+	}
+}
